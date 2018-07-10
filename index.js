d04f32a07aa037a2a48279d25989932838eee18f
momentspace <momentspace@gmail.com>
2018-07-10 14:05:09 +0900

    feat. post subscription and push notification

--- index.js
+++ index.js
@@ -2,11 +2,59 @@ const express = require('express')
 const app = express()
 const webpush = require('web-push');
 const vapidKeys = require('./keys.json')
+const bodyParser = require('body-parser');
+
+var subscription = null
+
+function sendNotification(payload) {
+  const options = {
+    // gcmAPIKey: '< GCM API Key >',
+    vapidDetails: Object.assign({}, vapidKeys, {subject: 'mailto: develop.sample100@gmail.com'})
+    // TTL: 100
+    // headers: {
+    //   '< header name >': '< header value >'
+    // },
+    // contentEncoding: '< Encoding type, e.g.: aesgcm or aes128gcm >',
+    // proxy: '< proxy server address >'
+  }
+  
+  console.log('-----')
+  console.log(subscription)
+  console.log(payload)
+  console.log(options)
+  console.log('-----')
+  webpush.sendNotification(
+    subscription,
+    payload,
+    options
+  )
+}
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  next()
+})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/key', (req, res) => {
   return res.json({
     publicKey: vapidKeys.publicKey
   })
+})
+
+app.post('/sub', (req, res) => {
+  console.log(req.body)
+  subscription = req.body
+  return subscription
+})
+
+app.post('/push', (req, res) => {
+  console.log(req.body)
+  sendNotification(req.body.text)
+  return req.body
 })
 
 
